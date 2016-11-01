b5994c2eb50351d0e60ece326ea744a2dc721398
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2016-11-01 18:09:52 +0100

    watch-free router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'welcome',  component: WelcomeComponent },
   { path: 'about',  component: AboutComponent },
   { path: 'subscribers',  component: SubscribersListComponent },
-  { path: 'watch',  component: WatchFreeComponent }
+  { path: 'watch-free',  component: WatchFreeComponent }
 ];
 
 @NgModule({
