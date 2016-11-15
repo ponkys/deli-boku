040bf0be3f5132b99c11be82256fd2fbe181300a
“ponkys” <diego.mosquera.soto@gmail.comssh-keygen -t rsa -C diego.mosquera.soto@gmail.comgit config --global color.ui truegit config --global user.name ponkysgit config --global user.email diego.mosquera.soto@gmail.com>
2016-11-15 18:35:12 +0100

    vime segurity improved in iframe

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,12 +18,6 @@ describe('App: Dokulatino', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/watch-free/watch-free.component.ts
+++ src/app/watch-free/watch-free.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DomSanitizer } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-watch-free',
   templateUrl: './watch-free.component.html',
   styleUrls: ['./watch-free.component.css']
 })
 export class WatchFreeComponent implements OnInit {
+  trustedUrl: any
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) {
+
+    this.trustedUrl = sanitizer.bypassSecurityTrustResourceUrl("https://player.vimeo.com/video/40915360");
+  }
 
   ngOnInit() {
+
   }
 
 }
